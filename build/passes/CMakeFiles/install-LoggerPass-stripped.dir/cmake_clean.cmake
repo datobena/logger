1a5e3b970f9787358151600e7d34668748a9c10e
datobena <dato.bena@gmail.com>
2025-10-30 01:08:13 +0000

    Initial commit fix

--- build/passes/CMakeFiles/install-LoggerPass-stripped.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/install-LoggerPass-stripped"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/install-LoggerPass-stripped.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/passes/CMakeFiles/install-LoggerPass.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/install-LoggerPass"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/install-LoggerPass.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
